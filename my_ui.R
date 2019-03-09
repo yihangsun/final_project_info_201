ebb35f18872155fe04161bb381f9fce1e4d3fe16
Yihang <ys95@uw.edu>
2019-03-09 12:14:16 -0800

    add heading

---
+++ my_ui.R
@@ -0,0 +1,9 @@
+my_ui <- fluidPage( 
+  titlePanel("Eastern and Western Crime Rate"),
+  h1("The crime rate of representive state in Eastern and Western States of 
+     U.S.A in past three decades"),
+  p("We are focusing on researching Washington State and New York State which  
+can represent most for East and West in United States. There are a lot of 
+important changes happened in past threee decades. We will explore those 
+changes in history along with criminal rates.")
+)
