bd632cf6ddcc5c0d90b737471bb199f3a4646715
Han Chen <chenhan@uw.edu>
2019-03-11 17:38:06 -0700

    readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# final_project_info_201
+# Eastern and Western Criminal Behavior Comparsion
+
+The crime rate of representive state in Eastern and Western States of U.S.A in past three decades.
+
+We are focusing on researching Washington State and New York State which can represent most for East and West in United States. There are a lot of important changes happened in past three decades. We will explore those changes in history along with criminal rates.
--- my_ui.R
+++ my_ui.R
@@ -8,11 +8,11 @@ ny_crime_report <- read.csv("Index__Violent__Property__and_Firearm_Rates_By_Coun
 
 my_ui <- fluidPage( 
   titlePanel("Eastern and Western Criminal Behavior Comparsion"),
-  h1("The crime rate of representive state in Eastern and Western States of 
+  h1("The crime rate of representative state in Eastern and Western States of 
      U.S.A in past three decades"),
   p("We are focusing on researching Washington State and New York State which  
 can represent most for East and West in United States. There are a lot of 
-important changes happened in past threee decades. We will explore those 
+important changes happened in past three decades. We will explore those 
 changes in history along with criminal rates."),
   
   sidebarLayout(
